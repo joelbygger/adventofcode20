77ca6cf94a63b3bd8510aa304234ffa40e2ba0fd
Joel Säll <joel.sael@gmail.com>
2020-12-04 16:48:31 +0100

    Cleaned day2 task 1.

--- src/Day2/main.cs
+++ src/Day2/main.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace Day2
 {
@@ -21,15 +20,12 @@ namespace Day2
             using var sr = new System.IO.StreamReader(args[0]);
             var pass = new Passwords(sr);
             int? sum = pass.countValid();
-            //using var sr = File.OpenRead(args[0]);
-            //int? sum = Passwords.work2(args[0]);
             if (sum != null) {
-                    Console.WriteLine("Task1: valid passowords: " + sum);
-                }
-                else {
-                    Console.WriteLine("Task1: No valid vals found.");
-                }
-            //}
+                Console.WriteLine("Task1: valid passowords: " + sum);
+            }
+            else {
+                Console.WriteLine("Task1: No valid vals found.");
+            }
         }
     }
 }
--- src/Day2/passwords.cs
+++ src/Day2/passwords.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.Linq;
 
 namespace Day2
 {
@@ -13,11 +12,9 @@ namespace Day2
         {
             _passwords = new PasswordDB();
 
+            // Read file and populate local storage.
             Regex dbFormat = new Regex(@"(\d+)-(\d+) (.): (.*)");
             string line;
-            //while (!sr.EndOfStream) {
-            //foreach (string line in sr.ReadLine()) {
-            //string line = sr.ReadLine();
             while ((line = sr.ReadLine()) != null) {
                 string[] aPass = dbFormat.Split(line);
                 // First and last index in found array is empty stirng, due to how Split works...
@@ -32,7 +29,7 @@ namespace Day2
         public int countValid()
         {
             int sum = 0;
-            foreach(var pass in _passwords) {
+            foreach (var pass in _passwords) {
                 Console.WriteLine(pass);
                 sum += isValid(pass) ? 1 : 0;
 
@@ -49,33 +46,6 @@ namespace Day2
             return (cnt <= pass.maxTimes && cnt >= pass.minTimes);
         }
 
-        /*
-        static public int? work2(string fileName)
-        {
-            // LINQ instead https://docs.microsoft.com/en-us/dotnet/api/system.io.file.readlines?view=net-5.0 ?
-            Regex rgx = new Regex(@"(\d+)-(\d+) (.): (.*)");
-
-            var store = new List<(int minTimes, int maxTimes, string chr, string pass)>();
-
-            var hej = System.IO.File.ReadLines(fileName);
-            var hej2 = System.IO.File.ReadAllLines(fileName);
-
-            var apa = from line1 in System.IO.File.ReadAllLines(fileName)
-                      from found0 in rgx.Split(line1)
-                      from found1 in found0
-                      select new {
-                          a = found1
-                      }
-                      into he
-                      select new { c = he };
-
-
-            foreach (var itm in apa) {
-                Console.WriteLine(" - " + itm.c);
-            }
-            return 0;
-        }*/
-
-        private PasswordDB _passwords;
+        private readonly PasswordDB _passwords;
     }
 }
